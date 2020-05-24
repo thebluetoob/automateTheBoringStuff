cdc1aa171b285160489cfd0407576523f37c6c55
Ben Ellett <github@benjaminellett.com>
2020-05-24 12:03:53 +1000

    add chapter 5

---
+++ chapter_5/catnapping.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping, cat burglary, and extortion.\n\nSincerely,\nBob')
